448d771aecfa561d3c8dc392ff888b88b3ffcddd
Adam Buczek <adambuczek@gmail.com>
2017-08-20 14:21:48 +0200

    feat (payu): add basic ordering

--- payu.class.js
+++ payu.class.js
@@ -4,17 +4,26 @@ const requestPromise = require('request-promise');
 const request = require('request');
 const fs = require('fs');
 
+const EventEmitter = require('events');
+const ErrorHandler = new EventEmitter();
+
+ErrorHandler.on('error', (err) => {
+  const errorLog = '.error.log';
+  fs.appendFile(errorLog, err, (e) => {
+    if (e) console.error(e);
+  });
+});
 // make promise version of fs.readFile()
-fs.readFileAsync = function(filename) {
-    return new Promise(function(resolve, reject) {
-        fs.readFile(filename, function(err, data){
-            if (err)
-                reject(err);
-            else
-                resolve(data);
-        });
-    });
-};
+// fs.readFileAsync = function(filename) {
+//     return new Promise(function(resolve, reject) {
+//         fs.readFile(filename, function(err, data){
+//             if (err)
+//                 reject(err);
+//             else
+//                 resolve(data);
+//         });
+//     });
+// };
 
 const PayU = function(settings) {
 
@@ -22,8 +31,13 @@ const PayU = function(settings) {
   this.clientSecret = settings.clientSecret;
 
   this.posId = settings.posId;
+  this.posDesc = settings.posDesc;
+  this.posCurrency = settings.posCurrency;
 
   this.signatureKey = settings.signatureKey;
+
+  this.notifyUrl = settings.notifyUrl;
+  this.continueUrl = settings.continueUrl;
 
   this.baseUrl = (settings.environment === 'secure') ? 'https://secure.payu.com' : 'https://secure.snd.payu.com';
   this.hash = (settings.hash) ? settings.hash : 'MD5';
@@ -35,37 +49,45 @@ const PayU = function(settings) {
   } else {
     this.grantType = 'client_credentials';
   }
+
 };
 
-PayU.prototype.authorize = async function () {
-    const url = '/pl/standard/user/oauth/authorize';
-    let now = Math.floor(Date.now() / 1000), //Get the current date in ms, convert to s and floor
-        cacheLocation = '.cache',
-        auth;
-    try {
-      auth = (fs.existsSync()) ? JSON.parse(fs.readFileSync(cacheLocation)) : false;
-      if (!auth || now >= auth.expires_at) {
-        let response = await requestPromise({
-          method: 'POST',
-          url: this.baseUrl + url,
-          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-          body: `grant_type=client_credentials&client_id=${ this.clientId }&client_secret=${ this.clientSecret }`
-        });
-        auth = JSON.parse(response);
-        auth.expires_at = now + auth.expires_in;
-        fs.writeFile(cacheLocation, JSON.stringify(auth), (err) => {if (err) throw new Exception(err)});
-      }
-      return auth.access_token;
-    } catch (e) {
-      console.error(e);
+PayU.prototype.authorize = async function() {
+  let now = Math.floor(Date.now() / 1000), //Get the current date in ms, convert to s and floor
+    cacheLocation = '.cache',
+    auth;
+  try {
+    auth = (fs.existsSync()) ? JSON.parse(fs.readFileSync(cacheLocation)) : false;
+    if (!auth || now >= auth.expires_at) {
+      const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      };
+      const url = '/pl/standard/user/oauth/authorize';
+      let response = await requestPromise({
+        method: 'POST',
+        url: this.baseUrl + url,
+        headers: headers,
+        body: `grant_type=client_credentials&client_id=${ this.clientId }&client_secret=${ this.clientSecret }`
+      });
+      auth = JSON.parse(response);
+      auth.expires_at = now + auth.expires_in;
+      fs.writeFile(cacheLocation, JSON.stringify(auth), (err) => {
+        if (err) throw new Exception(err)
+      });
     }
-
+    return auth.access_token;
+  } catch (e) {
+    ErrorHandler.emit('error', e);
+    throw new Error(400);
+  }
 };
 
-PayU.prototype.paymethods = async function () {
+PayU.prototype.paymethods = async function() {
   let auth = await this.authorize();
   const url = '/api/v2_1/paymethods/';
-  const headers = {'Authorization': `Bearer ${auth}`};
+  const headers = {
+    'Authorization': `Bearer ${auth}`
+  };
   try {
     let response = await requestPromise({
       method: 'GET',
@@ -74,7 +96,58 @@ PayU.prototype.paymethods = async function () {
     });
     return JSON.parse(response);
   } catch (e) {
-    console.error(e);
+    ErrorHandler.emit('error', e);
+  }
+};
+
+PayU.prototype.order = async function(req) {
+  let auth = await this.authorize();
+  const url = '/api/v2_1/orders/';
+  const headers = {
+    'Authorization': `Bearer ${auth}`,
+    'Content-Type': 'application/json'
+  };
+  const buyer = {        
+    email: "john.doe@example.com",
+    phone: "654111654",
+    firstName: "John",
+    lastName: "Doe",
+    language: "en"    
+  };
+  const products = [{
+    name: "Wireless Mouse for Laptop",
+    unitPrice: "15000",
+    quantity: "1"        
+  },{            
+    name: "HDMI cable",
+    unitPrice: "6000",
+    quantity: "1"        
+  }];
+  const odrer = {
+    notifyUrl: this.notifyUrl,
+    continueUrl: this.continueUrl,
+    customerIp: req.ip, // change this when behind the reverse proxy
+    merchantPosId: this.posId,
+    description: this.posDesc,
+    currencyCode: this.posCurrency,
+    totalAmount: products.reduce((acc, curr) => acc += parseInt(curr.unitPrice), 0),
+    products: products,
+    buyer: buyer,
+  };
+
+  try {
+
+    let response = await requestPromise({
+      method: 'POST',
+      url: this.baseUrl + url,
+      headers: headers,
+      body: JSON.stringify(odrer),
+      simple: false,
+    });
+    return JSON.parse(response);
+
+  } catch (e) {
+    ErrorHandler.emit('error', e);
   }
 };
 
--- server.js
+++ server.js
@@ -13,23 +13,47 @@ const port = process.env.PORT || 8080;
 
 const settings = {
   posId: 302060,
+  posDesc: "Jakiwół - ręcznie robiony na twoje zamówienie",
+  posCurrency: "PLN",
   clientId: 302060,
   clientSecret: 'db40979e497def5477a72128d3ecf696',
   signatureKey: 'b77aa26adb1fe6743b4fe1274b36e007',
   environment: 'sandbox',
+  notifyUrl: 'http://192.168.99.100:8080/notify',
+  continueUrl: 'http://192.168.99.100:8080',
 };
 
 let payu = new PayU(settings);
 
 // mongoose.connect('mongodb://mongo:27017');
 
-router.get('/', function(req, res) {});
+router.get('/', async function(req, res) {
+
+  let response = await requestPromise({
+    method: 'GET',
+    url: 'http://192.168.99.100:8080/order',
+    simple: false,
+  });
+
+  const url = JSON.parse(response).redirectUri;
+
+  res.send(`<a href="${url}">place order</a>`);
+
+});
 
 router.get('/paymethods', function(req, res) {
-  payu.paymethods().then((methods) => res.json(methods));
+  payu.paymethods().then((methods) => res.json(methods)).catch((err) => console.error(err));
+});
+
+router.get('/order', function(req, res) {
+  payu.order(req).then((link) => res.json(link)).catch((err) => console.error(err));
+});
+
+router.post('/notify', function(req, res) {
+  console.log('notify: ' + req);
 });
 
 app.use('/', router);
 app.listen(port);
 
-console.log('Magic happens on port ' + port);
+console.log('Server started on port ' + port);
