da8c4fbaa7c5cdddbcdce23e15bc0eec8c716f5a
Adam Buczek <adambuczek@gmail.com>
2017-08-30 00:19:53 +0200

    feat (test): add random ticket gen

---
+++ test/randomTicket.js
@@ -0,0 +1,15 @@
+const randomTicket = async function() {
+
+  const cart = await require('./randomCart.js')();
+  const user = await require('./randomUser.js')();
+
+return {
+    cart,
+    shippingMethod: '59a2cae4140889000ac408de',
+    user: user.id,
+    address: user.address
+  }
+
+};
+
+module.exports = randomTicket;
---
+++ test/randomUser.js
@@ -0,0 +1,37 @@
+const randomUser = async function() {
+
+  const db = await require('mongoose').connect('mongodb://mongo:27017', {useMongoClient: true});
+  const User = require('../models/user.js');
+  const requestPromise = require('request-promise');
+
+  const url = 'https://randomuser.me/api/';
+  const person = JSON.parse(await requestPromise({
+    method: 'GET',
+    url: url
+  })).results[0];
+
+  const address = {
+    recipient: `${person.name.first} ${person.name.last}`,
+    street: person.location.street,
+    city: person.location.city,
+    post: person.location.postcode,
+    country: person.nat,
+  }
+  const user = new User({
+    name: {
+      first: person.name.first,
+      last: person.name.last
+    },
+    email: person.email,
+  });
+
+  user.save();
+
+  return {
+    address,
+    id: user._id
+  }
+
+};
+
+module.exports = randomUser;
